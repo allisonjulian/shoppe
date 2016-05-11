e92754e095baf29bcb6da08accb3233ce63afe21
allisonjulian <masjulian13@gmail.com>
2016-05-11 19:15:29 +0800

    header naman ngayon

--- lib/shoppe/default_navigation.rb
+++ lib/shoppe/default_navigation.rb
@@ -13,9 +13,10 @@ Shoppe::NavigationManager.build(:admin_primary) do
   add_item :orders
   add_item :products
   add_item :product_categories
-  add_item :delivery_services
+  # add_item :delivery_services
   # add_item :tax_rates
   add_item :users
-  add_item :countries
+  # add_item :countries
+  add_item :calendar
   add_item :settings
 end
