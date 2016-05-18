bc49f4202a856cae3e8540b3470dbc03ad9602d9
allisonjulian <masjulian13@gmail.com>
2016-05-19 06:50:47 +0800

    debugging

--- lib/shoppe/default_navigation.rb
+++ lib/shoppe/default_navigation.rb
@@ -17,6 +17,6 @@ Shoppe::NavigationManager.build(:admin_primary) do
   # add_item :tax_rates
   add_item :users
   # add_item :countries
-  add_item :calendars
+  # add_item :calendars
   add_item :settings
 end
